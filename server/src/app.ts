97076bf29d7bf0462f8ab4622d57f4b2ab6d1116
manaskitta <manaskhandelwal15@gmail.com>
2025-06-27 09:52:38 +0530

    done

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));;
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://peak-coder.vercel.app/"],
+    origin: ["http://localhost:3000", process.env.FRONTEND_URL!],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
